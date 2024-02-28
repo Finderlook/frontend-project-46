cf0815bd6bb28f8608b0ff14ef08a42046a1ad2e
FinderWork <uoa-bm@yandex.ru>
2024-02-28 14:44:23 +0300

    Initial setup

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+#!/usr/bin/env node
+import { Command } from 'commander';
+const program = new Command();
+
+program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-v, --version', 'output the current version')
+    .helpOption('-h, --help', 'output usage information');
+
+
+program.parse();
